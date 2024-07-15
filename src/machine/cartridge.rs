0fa65d8213b2e461d9bf5837b5c4bda86ba16737
Fabio Forni <development@redaril.me>
2024-07-15 14:17:30 +0200

    cartridge: Implement MBC1

--- src/machine/cartridge.rs
+++ src/machine/cartridge.rs
@@ -5,65 +5,202 @@ use std::io::{self, Read, Seek, SeekFrom};
 use mbc::Mbc;
 
 pub struct Cartridge<R: Read + Seek> {
-    equipment: Equipment<R>,
+    hw: Hardware<R>,
     mbc: Mbc,
 }
 
 impl<R: Read + Seek> Cartridge<R> {
     pub fn new(mut data: R) -> io::Result<Self> {
-        let typ = cartridge_type(&mut data)?;
+        const TYPE: u16 = 0x147;
+        const ROM_SIZE: u16 = 0x148;
+        const RAM_SIZE: u16 = 0x149;
 
+        let typ = read_at(&mut data, TYPE)?;
+        let rom_size = read_at(&mut data, ROM_SIZE)?;
+        let ram_size = read_at(&mut data, RAM_SIZE)?;
         Ok(Self {
-            equipment: Equipment::new(data, typ),
+            hw: Hardware::from_cardridge_header(data, typ, rom_size, ram_size),
             mbc: Mbc::new(typ),
         })
     }
 }
 
-struct Rom<R: Read + Seek>(R);
+struct Rom<R: Read + Seek> {
+    data: R,
+    /// Number of banks composing the ROM.
+    banks: u8,
+    /// The currently selected ROM bank.
+    /// The default value is 1, since the first
+    /// [`Self::BANK_SIZE`] bytes are directly accessible.
+    curr_bank: u8,
+}
 
 impl<R: Read + Seek> Rom<R> {
+    /// Size, in bytes, of each ROM bank.
+    const BANK_SIZE: u16 = 16 * 1024;
+
+    fn new(data: R, banks: u8) -> Self {
+        Self {
+            data,
+            banks,
+            curr_bank: 1,
+        }
+    }
+
+    /// Reads data at an absolute address.
     fn at(&mut self, addr: u16) -> io::Result<u8> {
-        self.0.seek(SeekFrom::Start(addr as u64))?;
-        let mut buf = [0; 1];
-        self.0.read_exact(&mut buf)?;
-        Ok(buf[0])
+        read_at(&mut self.data, addr)
+    }
+
+    /// Reads data relative to the currently selected bank.
+    fn at_current_bank(&mut self, addr: u16) -> io::Result<u8> {
+        self.at(self.curr_bank as u16 * Self::BANK_SIZE + addr)
+    }
+
+    fn set_bank(&mut self, bank: u8) {
+        let mut bank = bank & 0b00011111; // The bank is addressed by 5 bits.
+        bank = if bank == 0 { 1 } else { bank };
+        // If bank number is too high, it is masked by the amount of bits
+        // required to represent the bank count.
+        bank &= (1 << self.banks.ilog2() as u8) - 1;
+
+        self.curr_bank = bank;
+    }
+
+    /// Sets the upper two bits of the current bank selection.
+    fn set_bank_extended(&mut self, ext: u8) {
+        let ext = ext & 0b00000011;
+        self.curr_bank = (self.curr_bank & 0b0011111) + (ext << 5);
     }
 }
 
-struct Equipment<R: Read + Seek> {
+struct Ram {
+    data: Vec<u8>,
+    banks: u8,
+    curr_bank: u8,
+    enabled: bool,
+}
+
+impl Ram {
+    /// Size, in bytes, of each RAM bank.
+    const BANK_SIZE: u16 = 8 * 1024;
+
+    fn new(banks: u8) -> Self {
+        Self {
+            data: vec![0; banks as usize * Self::BANK_SIZE as usize],
+            banks,
+            curr_bank: 0,
+            enabled: false,
+        }
+    }
+
+    /// Reads data at an absolute address.
+    fn read(&self, addr: u16) -> u8 {
+        if !self.enabled {
+            return 0xFF;
+        }
+        self.data[addr as usize]
+    }
+
+    /// Reads data relative to the currently selected bank.
+    fn read_current_bank(&mut self, addr: u16) -> u8 {
+        self.read(self.curr_bank as u16 * Self::BANK_SIZE + addr)
+    }
+
+    /// Writes data at an absolute address.
+    fn write(&mut self, addr: u16, val: u8) {
+        if !self.enabled {
+            return;
+        }
+        self.data[addr as usize] = val;
+    }
+
+    /// Writes data relative to the currently selected bank.
+    fn write_current_bank(&mut self, addr: u16, val: u8) {
+        self.write(self.curr_bank as u16 * Self::BANK_SIZE + addr, val)
+    }
+
+    fn set_bank(&mut self, bank: u8) {
+        self.curr_bank = bank & 0b00000011;
+    }
+}
+
+struct Hardware<R: Read + Seek> {
     rom: Rom<R>,
-    ram: Vec<u8>,
+    ram: Ram,
     /// Whether or not the cartridge sports a battery.
     /// The battery is used to retain values in RAM and/or
-    /// power the embedded RTC. For the sake of emulation,
+    /// power the embedded RTC. As far as the emulation is concerned,
     /// having a battery means having to store the cartridge state
     /// in a file.
     has_battery: bool,
+    banking_mode: BankingMode,
 }
 
-impl<R: Read + Seek> Equipment<R> {
-    fn new(data: R, typ: u8) -> Self {
-        let rom = Rom(data);
-        let ram = match typ {
-            _ => Vec::new(),
+impl<R: Read + Seek> Hardware<R> {
+    fn from_cardridge_header(data: R, typ: u8, rom_size: u8, ram_size: u8) -> Self {
+        let rom_banks = match rom_size {
+            0x00..=0x08 => (1 << rom_size) + 1,
+            0x52 => 72,
+            0x53 => 80,
+            0x54 => 96,
+            _ => unreachable!(),
+        };
+        let ram_banks = match ram_size {
+            0x00 => {
+                // MBC2 has 512 half-bytes or RAM, but it's internal, so ram_banks
+                // is technically zero. We don't emulate the hardware layout precisely,
+                // so we pretend it has 1 bank of regular RAM.
+                if (0x05..=0x06).contains(&typ) {
+                    1
+                } else {
+                    0
+                }
+            }
+            0x02 => 1,
+            0x03 => 4,
+            0x04 => 16,
+            0x05 => 8,
+            _ => unreachable!(),
         };
         let has_battery = match typ {
+            0x3 => true,
             _ => false,
         };
 
         Self {
-            rom,
-            ram,
+            rom: Rom::new(data, rom_banks),
+            ram: Ram::new(ram_banks),
             has_battery,
+            banking_mode: BankingMode::Rom,
         }
     }
 }
 
-fn cartridge_type<R: Read + Seek>(data: &mut R) -> io::Result<u8> {
-    const CARTRIDGE_TYPE: u64 = 0x147;
+/// Determines whether certain areas of a cartridge
+/// are mapped to a ROM bank or to a RAM bank.
+#[derive(PartialEq, Eq)]
+enum BankingMode {
+    /// Maps flexible areas of a cartridge to a ROM bank.
+    Rom = 0,
+    /// Maps flexible areas of a cartridge to a RAM bank.
+    Ram = 1,
+}
 
-    data.seek(SeekFrom::Start(CARTRIDGE_TYPE))?;
+impl TryFrom<u8> for BankingMode {
+    type Error = String;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(BankingMode::Rom),
+            1 => Ok(BankingMode::Ram),
+            _ => Err(format!("unsupported bank addressing value: {}", value)),
+        }
+    }
+}
+
+fn read_at<R: Read + Seek>(data: &mut R, addr: u16) -> io::Result<u8> {
+    data.seek(SeekFrom::Start(addr as u64))?;
     let mut buf = [0; 1];
     data.read_exact(&mut buf)?;
     Ok(buf[0])
--- src/machine/cartridge/mbc.rs
+++ src/machine/cartridge/mbc.rs
@@ -1,39 +1,94 @@
 use std::io::{self, Read, Seek};
 
+use super::{BankingMode, Hardware};
+
 /// The logic that a cartridge follows according to its hardware.
 pub enum Mbc {
     Mbc0,
+    Mbc1, // NOTE: this does not emulate MBC1M.
 }
 
 impl Mbc {
     pub fn new(typ: u8) -> Self {
         match typ {
-            0x0 | 0x8..=0x9 => Self::Mbc0 {},
+            0x00 | 0x08..=0x09 => Self::Mbc0,
+            0x01..=0x03 => Self::Mbc1,
             _ => todo!(),
         }
     }
 
-    pub fn read<R: Read + Seek>(
-        &self,
-        equipment: &mut super::Equipment<R>,
-        addr: u16,
-    ) -> io::Result<u8> {
+    pub fn read<R: Read + Seek>(&self, hw: &mut super::Hardware<R>, addr: u16) -> io::Result<u8> {
         match self {
-            Self::Mbc0 => mbc0_read(equipment, addr),
+            Self::Mbc0 => mbc0_read(hw, addr),
+            Self::Mbc1 => mbc1_read(hw, addr),
         }
     }
 
-    pub fn write<R: Read + Seek>(&mut self, _equipment: &mut super::Equipment<R>, _addr: u16) {
+    pub fn write<R: Read + Seek>(&mut self, hw: &mut Hardware<R>, addr: u16, val: u8) {
         match self {
-            _ => (),
+            Self::Mbc0 => (),
+            Self::Mbc1 => mbc1_write(hw, addr, val),
         }
     }
 }
 
-fn mbc0_read<R: Read + Seek>(equipment: &mut super::Equipment<R>, addr: u16) -> io::Result<u8> {
+fn mbc0_read<R: Read + Seek>(hw: &mut Hardware<R>, addr: u16) -> io::Result<u8> {
     match addr {
-        0x0000..=0x7FFF => equipment.rom.at(addr),
-        0xA000..=0xBFFF => Ok(equipment.ram[(addr - 0xA000) as usize]),
+        0x0000..=0x7FFF => hw.rom.at(addr),
+        0xA000..=0xBFFF => Ok(hw.ram.read(addr - 0xA000)),
+        _ => unreachable!(),
+    }
+}
+
+fn mbc1_read<R: Read + Seek>(hw: &mut Hardware<R>, addr: u16) -> io::Result<u8> {
+    match addr {
+        0x0000..=0x3FFF => hw.rom.at(addr),
+        0x4000..=0x7FFF => hw.rom.at_current_bank(addr - 0x4000),
+        0xA000..=0xBFFF => Ok(hw.ram.read_current_bank(addr - 0xA000)),
+        _ => unreachable!(),
+    }
+}
+
+fn mbc1_write<R: Read + Seek>(hw: &mut Hardware<R>, addr: u16, val: u8) {
+    match addr {
+        0x0000..=0x1FFF => hw.ram.enabled = val & 0x0F == 0x0A, // Any value with 0xA in the lower 4 bits enables the ram.
+        0x2000..=0x3FFF => hw.rom.set_bank(val),
+        0x4000..=0x5FFF => {
+            // This value is either the ROM bank selector (upper 2 bits),
+            // if at least 64 ROM banks are present, or the RAM bank selector.
+            if hw.rom.banks <= 32 && hw.ram.banks <= 1 {
+                // This cartridge is too small for bank selection to be meaningful.
+                return;
+            }
+            match hw.banking_mode {
+                BankingMode::Rom => hw.rom.set_bank_extended(val),
+                BankingMode::Ram => hw.ram.set_bank(val),
+            }
+        }
+        0x6000..=0x7FFF => {
+            if hw.rom.banks <= 32 && hw.ram.banks <= 1 {
+                // This cartridge is too small for banking mode to be meaningful.
+                return;
+            }
+            let new_mode = BankingMode::try_from(val).unwrap();
+            if hw.banking_mode == new_mode {
+                return;
+            }
+            match new_mode {
+                BankingMode::Rom => {
+                    hw.rom.set_bank_extended(hw.ram.curr_bank);
+                    hw.ram.set_bank(0);
+                }
+                BankingMode::Ram => {
+                    hw.ram.set_bank(hw.rom.curr_bank & 0b11000000 >> 6);
+                    hw.rom.set_bank_extended(0);
+                }
+            }
+            hw.banking_mode = new_mode;
+        }
+        0xA000..=0xBFFF => {
+            hw.ram.write_current_bank(addr - 0x4000, val);
+        }
         _ => unreachable!(),
     }
 }
