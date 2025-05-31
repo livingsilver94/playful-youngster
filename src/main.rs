e4121a4db8ea2f8d1d07ff900c21dc3b08ea457e
Fabio Forni <development@redaril.me>
2025-05-31 13:08:29 +0200

    main: Load cartridge

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{fs::File, io, sync::mpsc};
+use std::{
+    fs::File,
+    io::{self, BufReader},
+    sync::mpsc,
+};
 
 use cpal::traits::{DeviceTrait, HostTrait};
 use playful_youngster::{
@@ -19,8 +23,12 @@ const AUDIO_BUFFER_SIZE: usize = 1024;
 fn main() -> Result<(), Error> {
     let cartridge = Box::new(File::open("/tmp/cart")?);
 
+    let mut app = Application::new()?;
+    app.emulator
+        .insert_cartridge(Cartridge::new_from_header(cartridge)?);
+
     let evtloop = EventLoop::new()?;
-    evtloop.run_app(&mut Application::new()?)?;
+    evtloop.run_app(&mut app)?;
 
     Ok(())
 }
